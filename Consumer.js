887173d9c071dddb753f31e6b6bbba5c5ce7ee4c
Victor Felder <victor@draft.li>
2019-04-08 15:06:49 +0200

    refactor to add logging and properly destroy streams

--- Consumer.js
+++ Consumer.js
@@ -2,21 +2,25 @@ const { KafkaConsumer } = require('node-rdkafka')
 const { Readable } = require('readable-stream')
 
 class Consumer extends Readable {
-  constructor (host, topics, { group, raw = false } = {}) {
+  constructor ({ host, topics, logger, group, keepalive = true, raw = false } = {}) {
     super({ objectMode: true })
 
     this.consumer = KafkaConsumer.createReadStream({
       'group.id': group,
-      'metadata.broker.list': host
-    }, null, { topics })
+      'metadata.broker.list': host,
+      'socket.keepalive.enable': keepalive
+    }, {}, { topics })
 
-    this.consumer.on('data', async message => {
+    this.consumer.on('close', () => {
+      logger.info('KafkaConsumerStream closed')
+    })
+    this.consumer.on('data', logger.info)
+
+    this.consumer.on('data', (message) => {
       if (raw) {
         this.push(message)
-      } else {
-        if (message.value) {
-          this.push(message.value)
-        }
+      } else if (message.value) {
+        this.push(message.value)
       }
     })
   }
@@ -24,7 +28,7 @@ class Consumer extends Readable {
   _read () {}
 
   _destroy () {
-    this.consumer.close()
+    this.consumer.destroy()
   }
 }
 
